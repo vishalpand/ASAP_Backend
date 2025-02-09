8818dca00c2f98a694e37b83b44b03d1cd6036ad
vishalpand <vishalpandey0070@gmail.com>
2025-02-09 16:10:59 +0530

    database and myconfig

--- src/main/java/com/spring/asap/util/MyConfig.java
+++ src/main/java/com/spring/asap/util/MyConfig.java
@@ -6,15 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
-@Order(0) 
+@Order(0)
 public class MyConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8100")
+                .allowedOrigins("http://localhost:8100", "https://asap.up.railway.app") // 
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
