461461512e1579f8387dc77ba0cf7a776f976e61
vishalpand <vishalpandey0070@gmail.com>
2025-02-18 00:53:51 +0530

    allow

--- src/main/java/com/spring/asap/util/MyConfig.java
+++ src/main/java/com/spring/asap/util/MyConfig.java
@@ -10,10 +10,10 @@ public class MyConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8100", "https://asap.up.railway.app", "capacitor://localhost")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedOrigins("http://localhost:8100", "https://asap.up.railway.app", "capacitor://localhost") // Explicit origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true) // Allow credentials
+                .maxAge(3600); // Cache the preflight response for 1 hour
     }
 }
